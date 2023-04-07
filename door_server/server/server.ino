f8017f011e33a77179f9144b34cf1b75eb725cb5
915_랩실컴 <yoon6763@naver.com>
2023-04-07 16:15:14 +0900

    문 서버측 블루투스 통신 기능 업데이트

--- door_server/server/server.ino
+++ door_server/server/server.ino
@@ -5,23 +5,30 @@
 #include <stdlib.h>
 #include <SPI.h>
 #include <Ethernet.h>
+#include <SoftwareSerial.h>
 
 // 와이파이명
 const char* ssid = constant_ssid;
 const char* password = constant_password;
 
-WiFiServer server(5555);
+WiFiServer server(constant_port);
+
+// 블루투스
+SoftwareSerial btSerial (D5, D6);
 
 // 서보 모터
 Servo servo;
 int degree = 0; // 모터의 각도
-int isDoorOpen = 0;
+int isDoorOpen = 0; // 1 - 열림, 0 - 닫힘
+int lightState = 0; // 1 - 켜짐, 1 - 꺼짐
 
 void setup() {
   servo.attach(D1);
 
   // put your setup code here, to run once:
   Serial.begin(115200);
+  btSerial.begin(9600);
+
   delay(1000);
 
   WiFi.begin(ssid, password);
@@ -43,64 +50,89 @@ void setup() {
 }
 
 void loop() {
+  if(btSerial.available()) {
+    int command = btSerial.read();
+    Serial.print("Bluetooth Input : ");
+    Serial.println(command);
+    
+    if(command == '1') {
+      lightOn();
+    } else if(command == '0') {
+      lightOff();
+    }
+  } else {
+    WiFiClient client = server.available();
+    int newState = -1;
 
-  WiFiClient client = server.available();
-  int newState = -1;
+    if(client) {
+      Serial.println("Client Connected");
 
-  if(client) {
-    Serial.println("Client Connected");
+      String string = "";
 
-    String string = "";
+      //모든 글자 읽어오기
+      while(client.connected()) {
+        if(client.available()) {
+          char c = client.read();
+          string += c;
+        }
+      }
 
-    //모든 글자 읽어오기
-    while(client.connected()) {
-      if(client.available()) {
-        char c = client.read();
-        string += c;
+      Serial.println(string);
+
+      // string to float
+      float distance = string.toFloat();
+
+
+      if(distance > door_state_standard) {
+        // 열림
+        newState = 1;
+      } else {
+        // 닫힘
+        newState = 0;
       }
     }
 
-    Serial.println(string);
+    Serial.print("isDoorOpen : ");
+    Serial.println(isDoorOpen);
+    Serial.print("newState : ");
+    Serial.println(newState);
 
-    // string to float
-    float distance = string.toFloat();
+    if(newState != -1 && isDoorOpen != newState) {
+      Serial.println("Point0");
+      if(newState == 1) {
+        lightOn();
+      } else {
+        lightOff();
+      }
 
-//    isDoorOpen
-
-
-    if(distance > 7) {
-      newState = 1;
+      isDoorOpen = newState;
+    }
     
-    } else {
-      newState = 0;
 
-    }
+    client.stop();
+    Serial.println("Client Disconnected");
+    delay(200);
   }
-
-  Serial.print("isDoorOpen : ");
-  Serial.println(isDoorOpen);
-  Serial.print("newState : ");
-  Serial.println(newState);
-
-  if(newState != -1 && isDoorOpen != newState) {
-    Serial.println("Point0");
-    if(newState == 1) {
-      servo.write(0);
-      delay(1200);
-      servo.write(90);
-      Serial.println("Point1");
-    } else {
-      servo.write(170);
-      delay(1200);
-      servo.write(90);
-      Serial.println("Point2");
-    }
-
-    isDoorOpen = newState;
-  }
-  
-
-  client.stop();
-  Serial.println("Client Disconnected");
-  delay(200);
 }
+
+void lightOn() {
+  servo.write(0);
+  delay(1200);
+  servo.write(90);
+  Serial.println("Point1");
+
+  lightState = 1;
+}
+
+
+void lightOff() {
+  servo.write(170);
+  delay(1200);
+  servo.write(90);
+  Serial.println("Point2");
+
+  lightState = 0;
+}
+
+
+
